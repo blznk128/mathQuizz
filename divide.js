1dc406e7bfadb2747f4e1bc3110cf371351fa423
Benny Borunda <blznk128@yahoo.com>
2019-02-25 11:36:08 -0700

    added css

--- divide.js
+++ divide.js
@@ -1,119 +1,119 @@
 // determines the math problem
-let multi1 = Math.floor((Math.random() * 11) +1);
-let multi2 = Math.floor((Math.random() * 11) +1);
-let multi3 = Math.floor((Math.random() * 11) +1);
-let multi4 = Math.floor((Math.random() * 11) +1);
-let multi5 = Math.floor((Math.random() * 11) +1);
-let multi6 = Math.floor((Math.random() * 11) +1);
-let multi7 = Math.floor((Math.random() * 11) +1);
-let multi8 = Math.floor((Math.random() * 11) +1);
-let multi9 = Math.floor((Math.random() * 11) +1);
-let multi10 = Math.floor((Math.random() * 11) +1);
-let multi11 = Math.floor((Math.random() * 11) +1);
-let multi12 = Math.floor((Math.random() * 11) +1);
-let multi13 = Math.floor((Math.random() * 11) +1);
-let multi14 = Math.floor((Math.random() * 11) +1);
-let multi15 = Math.floor((Math.random() * 11) +1);
-let multi16 = Math.floor((Math.random() * 11) +1);
-let multi17 = Math.floor((Math.random() * 11) +1);
-let multi18 = Math.floor((Math.random() * 11) +1);
-let multi19 = Math.floor((Math.random() * 11) +1);
-let multi20 = Math.floor((Math.random() * 11) +1);
+let divide1 = Math.floor((Math.random() * 11) +1);
+let divide2 = Math.floor((Math.random() * 11) +1);
+let divide3 = Math.floor((Math.random() * 11) +1);
+let divide4 = Math.floor((Math.random() * 11) +1);
+let divide5 = Math.floor((Math.random() * 11) +1);
+let divide6 = Math.floor((Math.random() * 11) +1);
+let divide7 = Math.floor((Math.random() * 11) +1);
+let divide8 = Math.floor((Math.random() * 11) +1);
+let divide9 = Math.floor((Math.random() * 11) +1);
+let divide10 = Math.floor((Math.random() * 11) +1);
+let divide11 = Math.floor((Math.random() * 11) +1);
+let divide12 = Math.floor((Math.random() * 11) +1);
+let divide13 = Math.floor((Math.random() * 11) +1);
+let divide14 = Math.floor((Math.random() * 11) +1);
+let divide15 = Math.floor((Math.random() * 11) +1);
+let divide16 = Math.floor((Math.random() * 11) +1);
+let divide17 = Math.floor((Math.random() * 11) +1);
+let divide18 = Math.floor((Math.random() * 11) +1);
+let divide19 = Math.floor((Math.random() * 11) +1);
+let divide20 = Math.floor((Math.random() * 11) +1);
 
 
 //sets the numbers to the screen
-$('#multiply').append(multi1 + " " + " * " + multi2 + " = " + "<input id = 'multiProblemOne'></input>");
-$('#multiply').append('<br>' + multi3 + " " + " * " + multi4 + " = " + "<input id = 'multiProblemTwo'></input>");
-$('#multiply').append('<br>' + multi5 + " " + " * " + multi6 + " = " + "<input id = 'multiProblemThree'></input>");
-$('#multiply').append('<br>' + multi7 + " " + " * " + multi8 + " = " + "<input id = 'multiProblemFour'></input>");
-$('#multiply').append('<br>' + multi9 + " " + " * " + multi10 + " = " + "<input id = 'multiProblemFive'></input>");
-$('#multiply').append('<br>' + multi11 + " " + " * " + multi12 + " = " + "<input id = 'multiProblemSix'></input>");
-$('#multiply').append('<br>' + multi13 + " " + " * " + multi14 + " = " + "<input id = 'multiProblemSeven'></input>");
-$('#multiply').append('<br>' + multi15 + " " + " * " + multi16 + " = " + "<input id = 'multiProblemEight'></input>");
-$('#multiply').append('<br>' + multi17 + " " + " * " + multi18 + " = " + "<input id = 'multiProblemNine'></input>");
-$('#multiply').append('<br>' + multi19 + " " + " * " + multi20 + " = " + "<input id = 'multiProblemTen'></input>");
+$('#divide').append(divide1 + " " + " / " + divide2 + " = " + "<input id = 'divideProblemOne'></input>");
+$('#divide').append('<br>' + divide3 + " " + " / " + divide4 + " = " + "<input id = 'divideProblemTwo'></input>");
+$('#divide').append('<br>' + divide5 + " " + " / " + divide6 + " = " + "<input id = 'divideProblemThree'></input>");
+$('#divide').append('<br>' + divide7 + " " + " / " + divide8 + " = " + "<input id = 'divideProblemFour'></input>");
+$('#divide').append('<br>' + divide9 + " " + " / " + divide10 + " = " + "<input id = 'divideProblemFive'></input>");
+$('#divide').append('<br>' + divide11 + " " + " / " + divide12 + " = " + "<input id = 'divideProblemSix'></input>");
+$('#divide').append('<br>' + divide13 + " " + " / " + divide14 + " = " + "<input id = 'divideProblemSeven'></input>");
+$('#divide').append('<br>' + divide15 + " " + " / " + divide16 + " = " + "<input id = 'divideProblemEight'></input>");
+$('#divide').append('<br>' + divide17 + " " + " / " + divide18 + " = " + "<input id = 'divideProblemNine'></input>");
+$('#divide').append('<br>' + divide19 + " " + " / " + divide20 + " = " + "<input id = 'divideProblemTen'></input>");
 
 
 
 //users clicks done, calculates Correct and Wrong answers
-$('#multiDone').on('click', function() {
+$('#divideDone').on('click', function() {
 event.preventDefault();
-let multiProblemOne = parseInt($('#multiProblemOne').val());
-let multiProblemTwo = parseInt($('#multiProblemTwo').val());
-let multiProblemThree = parseInt($('#multiProblemThree').val());
-let multiProblemFour = parseInt($('#multiProblemFour').val());
-let multiProblemFive = parseInt($('#multiProblemFive').val());
-let multiProblemSix = parseInt($('#multiProblemSix').val());
-let multiProblemSeven = parseInt($('#multiProblemSeven').val());
-let multiProblemEight = parseInt($('#multiProblemEight').val());
-let multiProblemNine = parseInt($('#multiProblemNine').val());
-let multiProblemTen = parseInt($('#multiProblemTen').val());
+let divideProblemOne = parseInt($('#divideProblemOne').val());
+let divideProblemTwo = parseInt($('#divideProblemTwo').val());
+let divideProblemThree = parseInt($('#divideProblemThree').val());
+let divideProblemFour = parseInt($('#divideProblemFour').val());
+let divideProblemFive = parseInt($('#divideProblemFive').val());
+let divideProblemSix = parseInt($('#divideProblemSix').val());
+let divideProblemSeven = parseInt($('#divideProblemSeven').val());
+let divideProblemEight = parseInt($('#divideProblemEight').val());
+let divideProblemNine = parseInt($('#divideProblemNine').val());
+let divideProblemTen = parseInt($('#divideProblemTen').val());
 
-let multiCorrect = 0;
-let multiWrong = 0;
+let divideCorrect = 0;
+let divideWrong = 0;
 
- if (multiProblemOne === multi1 * multi2) {
-   multiCorrect ++
+ if (divideProblemOne === divide1 / divide2) {
+   divideCorrect ++
    
- } else if (multiProblemOne != multi1 * multi2){
-  multiWrong ++
+ } else if (divideProblemOne != divide1 / divide2){
+  divideWrong ++
  }
 
- if (multiProblemTwo === multi3 * multi4) {
-     multiCorrect ++
- } else if (multiProblemTwo != multi3 * multi4){
-    multiWrong ++
+ if (divideProblemTwo === divide3 / divide4) {
+     divideCorrect ++
+ } else if (divideProblemTwo != divide3 / divide4){
+    divideWrong ++
  }
 
- if(multiProblemThree === multi5 * multi6) {
-     multiCorrect++
- } else if (multiProblemThree != multi5 * multi6){
-     multiWrong ++
+ if(divideProblemThree === divide5 / divide6) {
+     divideCorrect++
+ } else if (divideProblemThree != divide5 / divide6){
+     divideWrong ++
  }
 
- if(multiProblemFour === multi7 * multi8) {
-    multiCorrect++
-} else if (multiProblemFour != multi7 * multi8){
-    multiWrong ++
+ if(divideProblemFour === divide7 / divide8) {
+    divideCorrect++
+} else if (divideProblemFour != divide7 / divide8){
+    divideWrong ++
 }
 
-if(multiProblemFive === multi9 * multi10) {
-    multiCorrect++
-} else if (multiProblemFive != multi9 * multi10){
-    multiWrong ++
+if(divideProblemFive === divide9 / divide10) {
+    divideCorrect++
+} else if (divideProblemFive != divide9 / divide10){
+    divideWrong ++
 }
 
-if(multiProblemSix === multi11 * multi12) {
-    multiCorrect++
-} else if (multiProblemSix != multi11 * multi12){
-    multiWrong ++
+if(divideProblemSix === divide11 / divide12) {
+    divideCorrect++
+} else if (divideProblemSix != divide11 / divide12){
+    divideWrong ++
 }
 
-if(multiProblemSeven === multi13 * multi14) {
-    multiCorrect++
-} else if (multiProblemSeven != multi13 * multi14){
-    multiWrong ++
+if(divideProblemSeven === divide13 / divide14) {
+    divideCorrect++
+} else if (divideProblemSeven != divide13 / divide14){
+    divideWrong ++
 }
 
-if(multiProblemEight === multi15 * multi16) {
-    multiCorrect++
-} else if (multiProblemEight != multi15 * multi16){
-    multiWrong ++
+if(divideProblemEight === divide15 / divide16) {
+    divideCorrect++
+} else if (divideProblemEight != divide15 / divide16){
+    divideWrong ++
 }
 
-if(multiProblemNine === multi17 * multi18) {
-    multiCorrect++
-} else if (multiProblemNine != multi17 * multi18){
-    multiWrong ++
+if(divideProblemNine === divide17 / divide18) {
+    divideCorrect++
+} else if (divideProblemNine != divide17 / divide18){
+    divideWrong ++
 }
 
-if(multiProblemTen === multi19 * multi20) {
-    multiCorrect++
-} else if (multiProblemTen != multi19 * multi20){
-    multiWrong ++
+if(divideProblemTen === divide19 / divide20) {
+    divideCorrect++
+} else if (divideProblemTen != divide19 / divide20){
+    divideWrong ++
 }
 
 
- $('#multiCorrect').text(multiCorrect)
- $('#multiWrong').text(multiWrong)
+ $('#divideCorrect').text(divideCorrect)
+ $('#divideWrong').text(divideWrong)
 })
